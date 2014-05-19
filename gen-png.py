8f8b51a4256163141d8673c7656d989487456f1a
idning <idning@gmail.com>
2014-05-19 09:19:28 +0800

    add gen-png.py

---
+++ gen-png.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+import time
+import copy
+import thread
+import logging
+from pcl import common
+
+import matplotlib.pyplot as plt
+
+#fig = plt.figure(figsize=(40, 50), dpi=100)
+
+fig, subplots = plt.subplots(4, 4)
+fig.set_size_inches(10, 6)
+fig.tight_layout()
+fig.subplots_adjust(left=0.125)
+subplots = [item for sublist in subplots for item in sublist]
+
+
+def get_field(d, f):
+    if f in d:
+        return d[f]
+    return 0
+
+def main():
+    lines = file('stat.log').readlines()
+    lines = [common.json_decode(line) for line in lines if line.startswith('{')]
+    lines = [line for line in lines if 'ts' in line]
+    fields = ['qps', 'cpu', 'du', 'mem-vms',
+            'mem-rss','avqqu-sz', 'avgrq-sz', 'util',
+            'w/s', 'wkB/s', 'wrqm/s', 'await',
+            'r/s', 'rkB/s', 'rrqm/s', 'svctm',
+            ]
+    #print fields
+    #x = [line['ts'] for line in lines]
+
+    for i, f in enumerate(fields):
+        p = subplots[i]
+        y = [get_field(line, f) for line in lines]
+        p.plot(y, 'r')
+        p.set_title(f)
+        plt.setp(p.get_xticklabels(), visible=False)
+    plt.savefig('test.png', dpi=300)
+
+if __name__ == "__main__":
+    main()
